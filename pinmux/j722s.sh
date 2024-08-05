2ff23cec323743c0b8dc170885c85725b749c296
Robert Nelson <robertcnelson@gmail.com>
2024-08-05 13:57:34 -0500

    pinmux: j722s: format gpio ranges
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- pinmux/j722s.sh
+++ pinmux/j722s.sh
@@ -25,104 +25,105 @@ echo "" >${file}-main-pinmux.txt
 echo "" >${file}-mcu-pinmux.txt
 echo "" >${file}-pins.csv
 echo "" >${file}-pins-gpio.txt
+reset_gpio
 
 number_pins=0
 number_holes=0
+offset=4000;
 
 pinctrl_single_pin_base=0
 gpio_ranges_line_start=0
 
-offset=4000;
 echo "#GPIO0," > ${file}-pins.csv
-label="GPIO0_0" ; ball="L24" ; sch=${label}; find_pin
-label="GPIO0_1" ; ball="L23" ; sch=${label}; find_pin
-label="GPIO0_2" ; ball="L22" ; sch=${label}; find_pin
-label="GPIO0_3" ; ball="K27" ; sch=${label}; find_pin
-label="GPIO0_4" ; ball="L27" ; sch=${label}; find_pin
-label="GPIO0_5" ; ball="L26" ; sch=${label}; find_pin
-label="GPIO0_6" ; ball="L25" ; sch=${label}; find_pin
-label="GPIO0_7" ; ball="L21" ; sch=${label}; find_pin
-label="GPIO0_8" ; ball="M26" ; sch=${label}; find_pin
-label="GPIO0_9" ; ball="N27" ; sch=${label}; find_pin
-label="GPIO0_10" ; ball="M27" ; sch=${label}; find_pin
-label="GPIO0_11" ; ball="K26" ; sch=${label}; find_pin
-label="GPIO0_12" ; ball="K23" ; sch=${label}; find_pin
-label="GPIO0_13" ; ball="K22" ; sch=${label}; find_pin
-label="GPIO0_14" ; ball="J22" ; sch=${label}; find_pin
-label="GPIO0_15" ; ball="R22" ; sch=${label}; find_pin
-label="GPIO0_16" ; ball="R23" ; sch=${label}; find_pin
-label="GPIO0_17" ; ball="R26" ; sch=${label}; find_pin
-label="GPIO0_18" ; ball="T27" ; sch=${label}; find_pin
-label="GPIO0_19" ; ball="T25" ; sch=${label}; find_pin
-label="GPIO0_20" ; ball="T24" ; sch=${label}; find_pin
-label="GPIO0_21" ; ball="T21" ; sch=${label}; find_pin
-label="GPIO0_22" ; ball="T22" ; sch=${label}; find_pin
-label="GPIO0_23" ; ball="U27" ; sch=${label}; find_pin
-label="GPIO0_24" ; ball="U26" ; sch=${label}; find_pin
-label="GPIO0_25" ; ball="V27" ; sch=${label}; find_pin
-label="GPIO0_26" ; ball="V25" ; sch=${label}; find_pin
-label="GPIO0_27" ; ball="V26" ; sch=${label}; find_pin
-label="GPIO0_28" ; ball="V24" ; sch=${label}; find_pin
-label="GPIO0_29" ; ball="V22" ; sch=${label}; find_pin
-label="GPIO0_30" ; ball="V23" ; sch=${label}; find_pin
-label="GPIO0_31" ; ball="T23" ; sch=${label}; find_pin
+label="GPIO0_0"; ball="L24"; sch=${label}; find_pin
+label="GPIO0_1"; ball="L23"; sch=${label}; find_pin
+label="GPIO0_2"; ball="L22"; sch=${label}; find_pin
+label="GPIO0_3"; ball="K27"; sch=${label}; find_pin
+label="GPIO0_4"; ball="L27"; sch=${label}; find_pin
+label="GPIO0_5"; ball="L26"; sch=${label}; find_pin
+label="GPIO0_6"; ball="L25"; sch=${label}; find_pin
+label="GPIO0_7"; ball="L21"; sch=${label}; find_pin
+label="GPIO0_8"; ball="M26"; sch=${label}; find_pin
+label="GPIO0_9"; ball="N27"; sch=${label}; find_pin
+label="GPIO0_10"; ball="M27"; sch=${label}; find_pin
+label="GPIO0_11"; ball="K26"; sch=${label}; find_pin
+label="GPIO0_12"; ball="K23"; sch=${label}; find_pin
+label="GPIO0_13"; ball="K22"; sch=${label}; find_pin
+label="GPIO0_14"; ball="J22"; sch=${label}; find_pin
+label="GPIO0_15"; ball="R22"; sch=${label}; find_pin
+label="GPIO0_16"; ball="R23"; sch=${label}; find_pin
+label="GPIO0_17"; ball="R26"; sch=${label}; find_pin
+label="GPIO0_18"; ball="T27"; sch=${label}; find_pin
+label="GPIO0_19"; ball="T25"; sch=${label}; find_pin
+label="GPIO0_20"; ball="T24"; sch=${label}; find_pin
+label="GPIO0_21"; ball="T21"; sch=${label}; find_pin
+label="GPIO0_22"; ball="T22"; sch=${label}; find_pin
+label="GPIO0_23"; ball="U27"; sch=${label}; find_pin
+label="GPIO0_24"; ball="U26"; sch=${label}; find_pin
+label="GPIO0_25"; ball="V27"; sch=${label}; find_pin
+label="GPIO0_26"; ball="V25"; sch=${label}; find_pin
+label="GPIO0_27"; ball="V26"; sch=${label}; find_pin
+label="GPIO0_28"; ball="V24"; sch=${label}; find_pin
+label="GPIO0_29"; ball="V22"; sch=${label}; find_pin
+label="GPIO0_30"; ball="V23"; sch=${label}; find_pin
+label="GPIO0_31"; ball="T23"; sch=${label}; find_pin
 found_hole ; mark_hole
-label="GPIO0_32" ; ball="N21" ; sch=${label}; find_pin
-label="GPIO0_33" ; ball="N22" ; sch=${label}; find_pin
-label="GPIO0_34" ; ball="N23" ; sch=${label}; find_pin
-label="GPIO0_35" ; ball="P27" ; sch=${label}; find_pin
-label="GPIO0_36" ; ball="P26" ; sch=${label}; find_pin
-label="GPIO0_37" ; ball="V21" ; sch=${label}; find_pin
-label="GPIO0_38" ; ball="W26" ; sch=${label}; find_pin
-label="GPIO0_39" ; ball="N24" ; sch=${label}; find_pin
-label="GPIO0_40" ; ball="N25" ; sch=${label}; find_pin
-label="GPIO0_41" ; ball="R27" ; sch=${label}; find_pin
-label="GPIO0_42" ; ball="P21" ; sch=${label}; find_pin
-label="GPIO0_43" ; ball="P22" ; sch=${label}; find_pin
-label="GPIO0_44" ; ball="P23" ; sch=${label}; find_pin
-label="GPIO0_45" ; ball="W27" ; sch=${label}; find_pin
-label="GPIO0_46" ; ball="W25" ; sch=${label}; find_pin
-label="GPIO0_47" ; ball="W24" ; sch=${label}; find_pin
-label="GPIO0_48" ; ball="W23" ; sch=${label}; find_pin
-label="GPIO0_49" ; ball="W22" ; sch=${label}; find_pin
-label="GPIO0_50" ; ball="W21" ; sch=${label}; find_pin
-label="GPIO0_51" ; ball="Y26" ; sch=${label}; find_pin
-label="GPIO0_52" ; ball="Y27" ; sch=${label}; find_pin
-label="GPIO0_53" ; ball="AA24" ; sch=${label}; find_pin
-label="GPIO0_54" ; ball="AA27" ; sch=${label}; find_pin
-label="GPIO0_55" ; ball="AA25" ; sch=${label}; find_pin
-label="GPIO0_56" ; ball="AB25" ; sch=${label}; find_pin
-label="GPIO0_57" ; ball="AA23" ; sch=${label}; find_pin
-label="GPIO0_58" ; ball="AA22" ; sch=${label}; find_pin
-label="GPIO0_59" ; ball="AB26" ; sch=${label}; find_pin
-label="GPIO0_60" ; ball="AB27" ; sch=${label}; find_pin
-label="GPIO0_61" ; ball="AB24" ; sch=${label}; find_pin
-label="GPIO0_62" ; ball="AC27" ; sch=${label}; find_pin
-label="GPIO0_63" ; ball="AB23" ; sch=${label}; find_pin
-label="GPIO0_64" ; ball="AC26" ; sch=${label}; find_pin
-label="GPIO0_65" ; ball="J27" ; sch=${label}; find_pin
-label="GPIO0_66" ; ball="H27" ; sch=${label}; find_pin
-label="GPIO0_67" ; ball="G27" ; sch=${label}; find_pin
-label="GPIO0_68" ; ball="G26" ; sch=${label}; find_pin
-label="GPIO0_69" ; ball="H26" ; sch=${label}; find_pin
+label="GPIO0_32"; ball="N21"; sch=${label}; find_pin
+label="GPIO0_33"; ball="N22"; sch=${label}; find_pin
+label="GPIO0_34"; ball="N23"; sch=${label}; find_pin
+label="GPIO0_35"; ball="P27"; sch=${label}; find_pin
+label="GPIO0_36"; ball="P26"; sch=${label}; find_pin
+label="GPIO0_37"; ball="V21"; sch=${label}; find_pin
+label="GPIO0_38"; ball="W26"; sch=${label}; find_pin
+label="GPIO0_39"; ball="N24"; sch=${label}; find_pin
+label="GPIO0_40"; ball="N25"; sch=${label}; find_pin
+label="GPIO0_41"; ball="R27"; sch=${label}; find_pin
+label="GPIO0_42"; ball="P21"; sch=${label}; find_pin
+label="GPIO0_43"; ball="P22"; sch=${label}; find_pin
+label="GPIO0_44"; ball="P23"; sch=${label}; find_pin
+label="GPIO0_45"; ball="W27"; sch=${label}; find_pin
+label="GPIO0_46"; ball="W25"; sch=${label}; find_pin
+label="GPIO0_47"; ball="W24"; sch=${label}; find_pin
+label="GPIO0_48"; ball="W23"; sch=${label}; find_pin
+label="GPIO0_49"; ball="W22"; sch=${label}; find_pin
+label="GPIO0_50"; ball="W21"; sch=${label}; find_pin
+label="GPIO0_51"; ball="Y26"; sch=${label}; find_pin
+label="GPIO0_52"; ball="Y27"; sch=${label}; find_pin
+label="GPIO0_53"; ball="AA24"; sch=${label}; find_pin
+label="GPIO0_54"; ball="AA27"; sch=${label}; find_pin
+label="GPIO0_55"; ball="AA25"; sch=${label}; find_pin
+label="GPIO0_56"; ball="AB25"; sch=${label}; find_pin
+label="GPIO0_57"; ball="AA23"; sch=${label}; find_pin
+label="GPIO0_58"; ball="AA22"; sch=${label}; find_pin
+label="GPIO0_59"; ball="AB26"; sch=${label}; find_pin
+label="GPIO0_60"; ball="AB27"; sch=${label}; find_pin
+label="GPIO0_61"; ball="AB24"; sch=${label}; find_pin
+label="GPIO0_62"; ball="AC27"; sch=${label}; find_pin
+label="GPIO0_63"; ball="AB23"; sch=${label}; find_pin
+label="GPIO0_64"; ball="AC26"; sch=${label}; find_pin
+label="GPIO0_65"; ball="J27"; sch=${label}; find_pin
+label="GPIO0_66"; ball="H27"; sch=${label}; find_pin
+label="GPIO0_67"; ball="G27"; sch=${label}; find_pin
+label="GPIO0_68"; ball="G26"; sch=${label}; find_pin
+label="GPIO0_69"; ball="H26"; sch=${label}; find_pin
 found_hole ; mark_hole
-label="GPIO0_70" ; ball="F27" ; sch=${label}; find_pin
-label="GPIO0_71" ; ball="F26" ; sch=${label}; find_pin
-label="GPIO0_72" ; ball="H21" ; sch=${label}; find_pin
-label="GPIO0_73" ; ball="AF25" ; sch=${label}; find_pin
-label="GPIO0_74" ; ball="AG26" ; sch=${label}; find_pin
-label="GPIO0_75" ; ball="AF27" ; sch=${label}; find_pin
-label="GPIO0_76" ; ball="AE23" ; sch=${label}; find_pin
-label="GPIO0_77" ; ball="AG25" ; sch=${label}; find_pin
-label="GPIO0_78" ; ball="AF24" ; sch=${label}; find_pin
-label="GPIO0_79" ; ball="AD23" ; sch=${label}; find_pin
-label="GPIO0_80" ; ball="AE27" ; sch=${label}; find_pin
-label="GPIO0_81" ; ball="AC25" ; sch=${label}; find_pin
-label="GPIO0_82" ; ball="AD27" ; sch=${label}; find_pin
-label="GPIO0_83" ; ball="AE24" ; sch=${label}; find_pin
-label="GPIO0_84" ; ball="AE26" ; sch=${label}; find_pin
-label="GPIO0_85" ; ball="AD25" ; sch=${label}; find_pin
-label="GPIO0_86" ; ball="AC24" ; sch=${label}; find_pin
+label="GPIO0_70"; ball="F27"; sch=${label}; find_pin
+label="GPIO0_71"; ball="F26"; sch=${label}; find_pin
+label="GPIO0_72"; ball="H21"; sch=${label}; find_pin
+label="GPIO0_73"; ball="AF25"; sch=${label}; find_pin
+label="GPIO0_74"; ball="AG26"; sch=${label}; find_pin
+label="GPIO0_75"; ball="AF27"; sch=${label}; find_pin
+label="GPIO0_76"; ball="AE23"; sch=${label}; find_pin
+label="GPIO0_77"; ball="AG25"; sch=${label}; find_pin
+label="GPIO0_78"; ball="AF24"; sch=${label}; find_pin
+label="GPIO0_79"; ball="AD23"; sch=${label}; find_pin
+label="GPIO0_80"; ball="AE27"; sch=${label}; find_pin
+label="GPIO0_81"; ball="AC25"; sch=${label}; find_pin
+label="GPIO0_82"; ball="AD27"; sch=${label}; find_pin
+label="GPIO0_83"; ball="AE24"; sch=${label}; find_pin
+label="GPIO0_84"; ball="AE26"; sch=${label}; find_pin
+label="GPIO0_85"; ball="AD25"; sch=${label}; find_pin
+label="GPIO0_86"; ball="AC24"; sch=${label}; find_pin
 found_hole
 
 pinctrl_single_pin_base=101
@@ -130,31 +131,31 @@ gpio_ranges_line_start=7
 
 echo "" >> ${file}-pins.csv
 echo "#GPIO1," >> ${file}-pins.csv
-label="GPIO1_7" ; ball="A25" ; sch=${label}; find_pin
-label="GPIO1_8" ; ball="A26" ; sch=${label}; find_pin
-label="GPIO1_9" ; ball="B25" ; sch=${label}; find_pin
-label="GPIO1_10" ; ball="F23" ; sch=${label}; find_pin
-label="GPIO1_11" ; ball="D25" ; sch=${label}; find_pin
-label="GPIO1_12" ; ball="C26" ; sch=${label}; find_pin
-label="GPIO1_13" ; ball="C27" ; sch=${label}; find_pin
-label="GPIO1_14" ; ball="F24" ; sch=${label}; find_pin
-label="GPIO1_15" ; ball="B20" ; sch=${label}; find_pin
-label="GPIO1_16" ; ball="C20" ; sch=${label}; find_pin
-label="GPIO1_17" ; ball="D20" ; sch=${label}; find_pin
-label="GPIO1_18" ; ball="E19" ; sch=${label}; find_pin
-label="GPIO1_19" ; ball="E20" ; sch=${label}; find_pin
-label="GPIO1_20" ; ball="F19" ; sch=${label}; find_pin
-label="GPIO1_21" ; ball="F20" ; sch=${label}; find_pin
-label="GPIO1_22" ; ball="E22" ; sch=${label}; find_pin
-label="GPIO1_23" ; ball="B21" ; sch=${label}; find_pin
-label="GPIO1_24" ; ball="D22" ; sch=${label}; find_pin
-label="GPIO1_25" ; ball="C22" ; sch=${label}; find_pin
-label="GPIO1_26" ; ball="D23" ; sch=${label}; find_pin
-label="GPIO1_27" ; ball="B22" ; sch=${label}; find_pin
-label="GPIO1_28" ; ball="C24" ; sch=${label}; find_pin
-label="GPIO1_29" ; ball="A22" ; sch=${label}; find_pin
-label="GPIO1_30" ; ball="A23" ; sch=${label}; find_pin
-label="GPIO1_31" ; ball="B23" ; sch=${label}; find_pin
+label="GPIO1_7"; ball="A25"; sch=${label}; find_pin
+label="GPIO1_8"; ball="A26"; sch=${label}; find_pin
+label="GPIO1_9"; ball="B25"; sch=${label}; find_pin
+label="GPIO1_10"; ball="F23"; sch=${label}; find_pin
+label="GPIO1_11"; ball="D25"; sch=${label}; find_pin
+label="GPIO1_12"; ball="C26"; sch=${label}; find_pin
+label="GPIO1_13"; ball="C27"; sch=${label}; find_pin
+label="GPIO1_14"; ball="F24"; sch=${label}; find_pin
+label="GPIO1_15"; ball="B20"; sch=${label}; find_pin
+label="GPIO1_16"; ball="C20"; sch=${label}; find_pin
+label="GPIO1_17"; ball="D20"; sch=${label}; find_pin
+label="GPIO1_18"; ball="E19"; sch=${label}; find_pin
+label="GPIO1_19"; ball="E20"; sch=${label}; find_pin
+label="GPIO1_20"; ball="F19"; sch=${label}; find_pin
+label="GPIO1_21"; ball="F20"; sch=${label}; find_pin
+label="GPIO1_22"; ball="E22"; sch=${label}; find_pin
+label="GPIO1_23"; ball="B21"; sch=${label}; find_pin
+label="GPIO1_24"; ball="D22"; sch=${label}; find_pin
+label="GPIO1_25"; ball="C22"; sch=${label}; find_pin
+label="GPIO1_26"; ball="D23"; sch=${label}; find_pin
+label="GPIO1_27"; ball="B22"; sch=${label}; find_pin
+label="GPIO1_28"; ball="C24"; sch=${label}; find_pin
+label="GPIO1_29"; ball="A22"; sch=${label}; find_pin
+label="GPIO1_30"; ball="A23"; sch=${label}; find_pin
+label="GPIO1_31"; ball="B23"; sch=${label}; find_pin
 found_hole ; mark_hole
 mark_hole
 mark_hole
@@ -170,15 +171,15 @@ mark_hole
 pinctrl_single_pin_base=137
 gpio_ranges_line_start=42
 
-label="GPIO1_42" ; ball="H25" ; sch=${label}; find_pin
-label="GPIO1_43" ; ball="J23" ; sch=${label}; find_pin
-label="GPIO1_44" ; ball="H20" ; sch=${label}; find_pin
-label="GPIO1_45" ; ball="H23" ; sch=${label}; find_pin
-label="GPIO1_46" ; ball="H24" ; sch=${label}; find_pin
+label="GPIO1_42"; ball="H25"; sch=${label}; find_pin
+label="GPIO1_43"; ball="J23"; sch=${label}; find_pin
+label="GPIO1_44"; ball="H20"; sch=${label}; find_pin
+label="GPIO1_45"; ball="H23"; sch=${label}; find_pin
+label="GPIO1_46"; ball="H24"; sch=${label}; find_pin
 found_hole ; mark_hole
-label="GPIO1_47" ; ball="H22" ; sch=${label}; find_pin
-label="GPIO1_48" ; ball="B24" ; sch=${label}; find_pin
-label="GPIO1_49" ; ball="A24" ; sch=${label}; find_pin
+label="GPIO1_47"; ball="H22"; sch=${label}; find_pin
+label="GPIO1_48"; ball="B24"; sch=${label}; find_pin
+label="GPIO1_49"; ball="A24"; sch=${label}; find_pin
 found_hole ; mark_hole
 mark_hole
 mark_hole
@@ -186,8 +187,8 @@ mark_hole
 pinctrl_single_pin_base=149
 gpio_ranges_line_start=50
 
-label="GPIO1_50" ; ball="E25" ; sch=${label}; find_pin
-label="GPIO1_51" ; ball="B27" ; sch=${label}; find_pin
+label="GPIO1_50"; ball="E25"; sch=${label}; find_pin
+label="GPIO1_51"; ball="B27"; sch=${label}; find_pin
 found_hole
 
 pinctrl_single_pin_base=0
@@ -195,34 +196,34 @@ gpio_ranges_line_start=0
 
 echo "" >> ${file}-pins.csv
 echo "#MCU_GPIO0," >> ${file}-pins.csv
-label="MCU_GPIO0_0" ; ball="C12" ; sch=${label}; find_pin
-label="MCU_GPIO0_1" ; ball="A10" ; sch=${label}; find_pin
-label="MCU_GPIO0_2" ; ball="A9" ; sch=${label}; find_pin
-label="MCU_GPIO0_3" ; ball="B12" ; sch=${label}; find_pin
-label="MCU_GPIO0_4" ; ball="C11" ; sch=${label}; find_pin
-label="MCU_GPIO0_5" ; ball="B8" ; sch=${label}; find_pin
-label="MCU_GPIO0_6" ; ball="B4" ; sch=${label}; find_pin
-label="MCU_GPIO0_7" ; ball="B5" ; sch=${label}; find_pin
-label="MCU_GPIO0_8" ; ball="C5" ; sch=${label}; find_pin
-label="MCU_GPIO0_9" ; ball="B3" ; sch=${label}; find_pin
-label="MCU_GPIO0_10" ; ball="C8" ; sch=${label}; find_pin
-label="MCU_GPIO0_11" ; ball="C4" ; sch=${label}; find_pin
-label="MCU_GPIO0_12" ; ball="C3" ; sch=${label}; find_pin
-label="MCU_GPIO0_13" ; ball="B2" ; sch=${label}; find_pin
-label="MCU_GPIO0_14" ; ball="D8" ; sch=${label}; find_pin
-label="MCU_GPIO0_15" ; ball="C1" ; sch=${label}; find_pin
-label="MCU_GPIO0_16" ; ball="B1" ; sch=${label}; find_pin
-label="MCU_GPIO0_17" ; ball="B13" ; sch=${label}; find_pin
-label="MCU_GPIO0_18" ; ball="E11" ; sch=${label}; find_pin
-label="MCU_GPIO0_19" ; ball="B9" ; sch=${label}; find_pin
-label="MCU_GPIO0_20" ; ball="D11" ; sch=${label}; find_pin
+label="MCU_GPIO0_0"; ball="C12"; sch=${label}; find_pin
+label="MCU_GPIO0_1"; ball="A10"; sch=${label}; find_pin
+label="MCU_GPIO0_2"; ball="A9"; sch=${label}; find_pin
+label="MCU_GPIO0_3"; ball="B12"; sch=${label}; find_pin
+label="MCU_GPIO0_4"; ball="C11"; sch=${label}; find_pin
+label="MCU_GPIO0_5"; ball="B8"; sch=${label}; find_pin
+label="MCU_GPIO0_6"; ball="B4"; sch=${label}; find_pin
+label="MCU_GPIO0_7"; ball="B5"; sch=${label}; find_pin
+label="MCU_GPIO0_8"; ball="C5"; sch=${label}; find_pin
+label="MCU_GPIO0_9"; ball="B3"; sch=${label}; find_pin
+label="MCU_GPIO0_10"; ball="C8"; sch=${label}; find_pin
+label="MCU_GPIO0_11"; ball="C4"; sch=${label}; find_pin
+label="MCU_GPIO0_12"; ball="C3"; sch=${label}; find_pin
+label="MCU_GPIO0_13"; ball="B2"; sch=${label}; find_pin
+label="MCU_GPIO0_14"; ball="D8"; sch=${label}; find_pin
+label="MCU_GPIO0_15"; ball="C1"; sch=${label}; find_pin
+label="MCU_GPIO0_16"; ball="B1"; sch=${label}; find_pin
+label="MCU_GPIO0_17"; ball="B13"; sch=${label}; find_pin
+label="MCU_GPIO0_18"; ball="E11"; sch=${label}; find_pin
+label="MCU_GPIO0_19"; ball="B9"; sch=${label}; find_pin
+label="MCU_GPIO0_20"; ball="D11"; sch=${label}; find_pin
 found_hole_mcu ; mark_hole
 mark_hole
 
 pinctrl_single_pin_base=23
 gpio_ranges_line_start=21
 
-label="MCU_GPIO0_21" ; ball="E13" ; sch=${label}; find_pin
+label="MCU_GPIO0_21"; ball="E13"; sch=${label}; find_pin
 found_hole_mcu ; mark_hole
 mark_hole
 mark_hole
@@ -234,9 +235,10 @@ mark_hole
 
 pinctrl_single_pin_base=32
 gpio_ranges_line_start=22
-label="MCU_GPIO0_22" ; ball="A8" ; sch=${label}; find_pin
-label="MCU_GPIO0_23" ; ball="F12" ; sch=${label}; find_pin
+label="MCU_GPIO0_22"; ball="A8"; sch=${label}; find_pin
+label="MCU_GPIO0_23"; ball="F12"; sch=${label}; find_pin
 found_hole_mcu
+finalize_gpio
 
 rm ${file}-pinmux.txt || true
 rm ${file}-main-pinmux.txt || true
